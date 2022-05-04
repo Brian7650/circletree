309a84e0fa9b23c7257fcedc08737c6ea9ef1cc3
Brian B <brtl567@gmail.com>
2022-05-04 01:56:52 -0400

    added app.post

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+});
+
 
 app.listen(3000, function(){
     console.log("Server is running in port 3000");
