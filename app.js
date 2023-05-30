2db40b7cb171d3fe6801166b1e5400ca844e2b57
Brian.B <brtl765@gmail.com>
2023-05-30 18:36:37 -0400

    used chatapi to fix errors

--- app.js
+++ app.js
@@ -1,69 +1,68 @@
 require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
 const https = require("https");
-const urlencoded = require("body-parser/lib/types/urlencoded");
-const { json } = require("express/lib/response");
 
 const app = express();
 
-console.log()
-
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/signup.html");
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
-                }
-            }
-        ]
-    };
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-    const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
 
-    const url = "https://us1.api.mailchimp.com/3.0/lists/af7b718103/";
-    const options = {
-        method: "POST",
-        auth: process.env.MAILCHIMP_API_KEY
+  const url = "https://us1.api.mailchimp.com/3.0/lists/af7b718103/";
+  const options = {
+    method: "POST",
+    auth: process.env.MAILCHIMP_API_KEY,
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
     }
 
-    const request = https.request(url, options, function(response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
 
-        if (response.statusCode === 200) {
-            res.sendFile(__dirname + "/success.html");
-        } else {
-            res.sendFile(__dirname + "/failure.html");
-        }
+  request.on("error", function (error) {
+    console.error(error);
+    res.sendFile(__dirname + "/failure.html");
+  });
 
-        response.on("data", function(data) {
-            console.log(JSON.parse(data));
-        })
-    })
-    request.write(jsonData);
-    request.end();
+  request.write(jsonData);
+  request.end();
 });
 
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
 
-app.post("/failure", function(req, res){
-    res.redirect("/")
-})
-
-app.listen(process.env.PORT || 8000, function(){
-    console.log("Server is running in port 8000");
+const port = process.env.PORT || 8000;
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
 });
